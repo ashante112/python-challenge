d7c5f13f2d4f19a7138f733963bbe33c05028d41
ashante112 <74433556+ashante112@users.noreply.github.com>
2021-01-27 22:43:45 -0500

    Update to main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,7 +10,9 @@ csvpath = os.path.join("Resources","budget_data.csv")
 # Set lists for retrieval
 months = []
 profit_losses = []
-# profit_loss_changes = []
+changes_in_profit_losses = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 999999999999999]
 
 # Open file to read (in read mode)
 with open(csvpath, 'r') as csvfile:
@@ -18,43 +20,51 @@ with open(csvpath, 'r') as csvfile:
     
     # Read header in first row
     csv_header = next(csvreader)
+    first_row = next(csvreader)
+    initial_net = int(first_row[1])
 
     for n in csvreader:
         month = n[0]
         months.append(month)
+        
         profit_loss = int(n[1])
         profit_losses.append(profit_loss)
 
-    
+        net_change = profit_loss - initial_net
+        initial_net = int(n[1])
+        changes_in_profit_losses.append(net_change)
+
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = n[0]
+            greatest_increase[1] = net_change
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = n[0]
+            greatest_decrease[1] = net_change
+
 total_months = len(months)
 net_profit_losses = sum(profit_losses)
+net_monthly_average = sum(changes_in_profit_losses)/len(changes_in_profit_losses)
 
 print(f"Financial Analysis")
 print(f"_______________________________")
 print(f"Total Months: {total_months}")
 print(f"Total: ${net_profit_losses}")
+print(f"Average Change: ${net_monthly_average}")
+print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
+print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
 
 # Create Output file
-output_path = os.path.join("output.txt")
+output_path = os.path.join("Analysis","output.txt")
 
 output=(
     f"\nFinancial Analysis\n"
     f"_______________________________\n"
     f"Total Months: {total_months}\n"
     f"Total: ${net_profit_losses}\n"
-    # f"Average Change: ${profit_loss_changes}\n"
-    # f"Greatest Increase In Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
-    # f"Greatest Decrease In Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n"
+    f"Average Change: ${net_monthly_average}\n"
+    f"Greatest Increase In Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease In Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n"
 )
 
 with open(output_path, 'w', newline="") as txtfile:
     txtfile.write(output)
-
-#Print Output  
-# txtfile.write(f"Financial Analysis")
-# txtfile.write(f"__________________________________")
-# txtfile.write(f"Total Months: ${total_months}")
-# txtfile.write(f"Total: " + "$"net_total)
-# txtfile.write(f"Average Change: " + avg_change)
-# txtfile.write(f"Greatest Increase in Profits: " + gipDate + "($"gip")")
-# txtfile.write(f"Greatest Decrease in Profits: " + gdpDate + "($"gdp")")
